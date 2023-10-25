353d9fbdb08402c2207289d43f168d6004011004
afra2604 <109967726+afra2604@users.noreply.github.com>
2023-10-25 22:45:54 +0700

    Second Commit

--- app/src/main/java/com/example/pertemuan9/HomeActivity.kt
+++ app/src/main/java/com/example/pertemuan9/HomeActivity.kt
@@ -4,26 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.example.pertemuan9.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         title = "Dashboard"
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         menuInflater.inflate(R.menu.menu_option, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_logout) {
-            finish()
-            return true
-        } else {
-            return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                finish()
+                Toast.makeText(this, "Berhasil Logout", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
         }
     }
 }
